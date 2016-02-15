a893fd89177513967cd312e0948cd49cefd1aa1b
galacticFlaneur <lawrencedr2@gmail.com>
2016-02-15 09:47:58 -0500

    extra files

--- rubyExamples.rb
+++ rubyExamples.rb
@@ -1,11 +1,26 @@
-puts "this is a test"
+ARGV.each do |line|
+	code_map = {"a" => 0, "b" => 1,"c" => 2,"d" => 3,"e" => 4,"f" => 5,"g" => 6,"h" => 7,"i" => 8,"j" => 9,
+				"0" => 0, "1" => 1,"2" => 2,"3" => 3,"4" => 4,"5" => 5,"6" => 6,"7" => 7,"8" => 8,"9" => 9 }
+	
+	tmp = line.split("")
+	
+	newstring = ""
 
-alpha = "a string"
-beta = 42
 
-puts alpha
-puts beta
+	for i in 0..tmp.size
+		a = tmp[i].to_s
+		if code_map.key?(a)
+			newstring = newstring + code_map.fetch(a).to_s
+		end
 
-puts alpha + " whose meaning is " + beta.to_s
+	end
 
-#this is a comment in ruby222222222
+	if newstring == ""
+		puts "NONE"
+	else
+		puts newstring
+	end
+
+
+end
+
